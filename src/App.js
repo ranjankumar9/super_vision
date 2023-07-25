4ce10c7ecbe038b7d5e47cc59ce6887bdf11a0c5
Ranjan Kumar <107936455+ranjankumar9@users.noreply.github.com>
2023-07-25 12:12:00 +0530

    Update App.js
    U

--- src/App.js
+++ src/App.js
@@ -1,82 +1,106 @@
-import React from 'react';
-import { useDrag, useDrop } from 'react-dnd';
-import { ItemTypes } from './ItemTypes'; // We'll define this later
+import React, { useState } from 'react';
+import './MatchingLine.css'; // Make sure to create this CSS file
 
-const Question = ({ question, onDrop }) => {
-  const [{ isDragging }, drag] = useDrag(() => ({
-    type: ItemTypes.QUESTION,
-    item: { question },
-    collect: (monitor) => ({
-      isDragging: !!monitor.isDragging(),
-    }),
-  }));
+const MatchingLine = () => {
+  const [matchedPairs, setMatchedPairs] = useState([]);
 
-  return (
-    <div
-      ref={drag}
-      style={{
-        opacity: isDragging ? 0.5 : 1,
-        cursor: 'move',
-        border: '1px solid #ccc',
-        padding: '10px',
-        margin: '5px',
-      }}
-    >
-      {question}
-    </div>
-  );
-};
-
-const Answer = ({ answer, onDrop }) => {
-  const [{ isOver }, drop] = useDrop(() => ({
-    accept: ItemTypes.QUESTION,
-    drop: (item) => onDrop(item.question, answer),
-    collect: (monitor) => ({
-      isOver: !!monitor.isOver(),
-    }),
-  }));
-
-  return (
-    <div
-      ref={drop}
-      style={{
-        backgroundColor: isOver ? '#f5f5f5' : 'transparent',
-        border: '1px solid #ccc',
-        padding: '10px',
-        margin: '5px',
-      }}
-    >
-      {answer}
-    </div>
-  );
-};
-
-const MatchQuestionAndAnswer = () => {
   const questions = ['Question 1', 'Question 2', 'Question 3'];
   const answers = ['Answer A', 'Answer B', 'Answer C'];
 
   const handleDrop = (question, answer) => {
-    console.log(`Question: ${question}, Answer: ${answer}`);
-    // Add your logic here to check if the match is correct or display feedback to the user
+    // Add the matched pair to the state
+    setMatchedPairs([...matchedPairs, { question, answer }]);
+  };
+
+  const removeMatch = (question) => {
+    // Remove the matched pair from the state
+    setMatchedPairs(matchedPairs.filter((pair) => pair.question !== question));
   };
 
   return (
-    <div style={{ display: 'flex' }}>
-      <div>
-        <h2>Questions</h2>
-        {questions.map((question, index) => (
-          <Question key={index} question={question} onDrop={handleDrop} />
-        ))}
+    <div>
+      <div className="matching-container">
+        <div className="questions">
+          {questions.map((question, index) => (
+            <div
+              key={index}
+              className="question-box"
+              onDrop={() => handleDrop(question, '')}
+              onDragOver={(e) => e.preventDefault()}
+            >
+              {question}
+            </div>
+          ))}
+        </div>
+        <div className="answers">
+          {answers.map((answer, index) => (
+            <div
+              key={index}
+              className="answer-box"
+              onDrop={() => handleDrop('', answer)}
+              onDragOver={(e) => e.preventDefault()}
+            >
+              {answer}
+            </div>
+          ))}
+        </div>
       </div>
-      <div>
-        <h2>Answers</h2>
-        {answers.map((answer, index) => (
-          <Answer key={index} answer={answer} onDrop={handleDrop} />
+      <div className="matching-lines">
+        {matchedPairs.map((pair, index) => (
+          <MatchingLineConnector
+            key={index}
+            question={pair.question}
+            answer={pair.answer}
+            onRemove={() => removeMatch(pair.question)}
+          />
         ))}
       </div>
     </div>
   );
 };
 
-export default MatchQuestionAndAnswer;
+const MatchingLineConnector = ({ question, answer, onRemove }) => {
+  const questionRef = React.createRef();
+  const answerRef = React.createRef();
 
+  const drawLine = () => {
+    const questionRect = questionRef.current.getBoundingClientRect();
+    const answerRect = answerRef.current.getBoundingClientRect();
+
+    const startX = questionRect.left + questionRect.width / 2;
+    const startY = questionRect.top + questionRect.height / 2;
+    const endX = answerRect.left + answerRect.width / 2;
+    const endY = answerRect.top + answerRect.height / 2;
+
+    const line = document.createElement('div');
+    line.className = 'matching-line';
+    line.style.position = 'absolute';
+    line.style.left = startX + 'px';
+    line.style.top = startY + 'px';
+    line.style.width = Math.sqrt((endX - startX) ** 2 + (endY - startY) ** 2) + 'px';
+    line.style.transformOrigin = '0 0';
+    line.style.transform = `rotate(${Math.atan2(endY - startY, endX - startX)}rad)`;
+
+    document.body.appendChild(line);
+
+    return line;
+  };
+
+  const line = drawLine();
+
+  return (
+    <React.Fragment>
+      <div ref={questionRef} className="hidden-element">
+        {question}
+      </div>
+      <div ref={answerRef} className="hidden-element">
+        {answer}
+      </div>
+      <div className="matching-remove" onClick={onRemove}>
+        X
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default MatchingLine;
