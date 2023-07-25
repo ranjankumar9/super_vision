a783d2135262ff43980c25edde2a3df7b80b6a72
Ranjan Kumar <107936455+ranjankumar9@users.noreply.github.com>
2023-07-25 16:08:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { useState } from 'react';
 import ReactFlow, { addEdge, removeElements } from 'react-flow-renderer';
 
-const FlowComponent = () => {
+const NodeBetweenButtons = () => {
   const [elements, setElements] = useState([]);
   const [count, setCount] = useState(0);
-
-  const onElementClick = (event, element) => {
-    console.log('Element clicked:', element);
-  };
 
   const onAddButtonClick = () => {
     const newNode = {
       id: `node-${count}`,
       data: { label: `Node ${count}` },
-      position: { x: 50 + count * 100, y: 50 + count * 100 },
+      position: { x: 100, y: 100 },
       type: 'default',
     };
 
@@ -25,30 +21,21 @@ const FlowComponent = () => {
     setElements((prevElements) => removeElements(elements, prevElements));
   };
 
-  const onElementsRemove = (elementsToRemove) => {
-    setElements((prevElements) => removeElements(elementsToRemove, prevElements));
-  };
-
   const onConnect = (params) => {
     setElements((prevElements) => addEdge(params, prevElements));
   };
 
   return (
     <div>
-      <div>
+      <div style={{ marginBottom: '10px' }}>
         <button onClick={onAddButtonClick}>Add Node</button>
         <button onClick={onRemoveButtonClick}>Remove Node</button>
       </div>
       <div style={{ height: '400px', border: '1px solid #ddd' }}>
-        <ReactFlow
-          elements={elements}
-          onElementClick={onElementClick}
-          onElementsRemove={onElementsRemove}
-          onConnect={onConnect}
-        />
+        <ReactFlow elements={elements} onConnect={onConnect} />
       </div>
     </div>
   );
 };
 
-export default FlowComponent;
+export default NodeBetweenButtons;
