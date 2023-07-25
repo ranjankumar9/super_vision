7a86a24fc8b97e7a0a4cb23a273f0f75e0531411
Ranjan Kumar <107936455+ranjankumar9@users.noreply.github.com>
2023-07-25 15:45:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
-import React from 'react';
-import './ButtonLine.css';
+import React, { useState } from 'react';
+import ReactFlow, { addEdge, removeElements } from 'react-flow-renderer';
 
-const ButtonLine = () => {
+const FlowComponent = () => {
+  const [elements, setElements] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const onElementClick = (event, element) => {
+    console.log('Element clicked:', element);
+  };
+
+  const onAddButtonClick = () => {
+    const newNode = {
+      id: `node-${count}`,
+      data: { label: `Node ${count}` },
+      position: { x: 50 + count * 100, y: 50 + count * 100 },
+      type: 'default',
+    };
+
+    setElements((prevElements) => [...prevElements, newNode]);
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const onRemoveButtonClick = () => {
+    setElements((prevElements) => removeElements(elements, prevElements));
+  };
+
+  const onElementsRemove = (elementsToRemove) => {
+    setElements((prevElements) => removeElements(elementsToRemove, prevElements));
+  };
+
+  const onConnect = (params) => {
+    setElements((prevElements) => addEdge(params, prevElements));
+  };
+
   return (
-    <div className="button-line-container">
-      <button className="btn1">Button 1</button>
-      <svg className="line">
-        <line x1="0" y1="0" x2="100" y2="0" />
-      </svg>
-      <button className="btn2">Button 2</button>
+    <div>
+      <div>
+        <button onClick={onAddButtonClick}>Add Node</button>
+        <button onClick={onRemoveButtonClick}>Remove Node</button>
+      </div>
+      <div style={{ height: '400px', border: '1px solid #ddd' }}>
+        <ReactFlow
+          elements={elements}
+          onElementClick={onElementClick}
+          onElementsRemove={onElementsRemove}
+          onConnect={onConnect}
+        />
+      </div>
     </div>
   );
 };
 
-export default ButtonLine;
+export default FlowComponent;
